a580d2bed49019646bc38253ea934bc4bbe89445
Vimax <it-vimax@gmail.com>
2020-02-24 23:49:16 +0200

    Finish 1 less. Install drawable import plugin

---
+++ app/src/main/java/app/vimax/androiddrinkshop/Utils/Common.java
@@ -0,0 +1,13 @@
+package app.vimax.androiddrinkshop.Utils;
+
+import app.vimax.androiddrinkshop.Retrofit.IDrinkShopAPI;
+import app.vimax.androiddrinkshop.Retrofit.RetrofitClient;
+
+public class Common {
+    private static final String BASE_URL = "http://wos.kl.com.ua";
+
+    public static IDrinkShopAPI getAPI()
+    {
+        return RetrofitClient.getClient(BASE_URL).create(IDrinkShopAPI.class);
+    }
+}
