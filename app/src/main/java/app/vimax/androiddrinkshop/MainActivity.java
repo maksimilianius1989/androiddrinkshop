3b31332266f2adbbcb2c5a9f2ff31a882c6700c9
Vimax <it-vimax@gmail.com>
2020-02-26 20:38:55 +0200

    LESSONS 6. Fix to run app on device.

--- app/src/main/java/app/vimax/androiddrinkshop/MainActivity.java
+++ app/src/main/java/app/vimax/androiddrinkshop/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -84,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onFailure(Call<CheckUserResponse> call, Throwable t) {
-
+                        Log.e("MYERROR", "DISCONNECT TO SERVER");
                     }
                 });
 
