e100b546bb5d3508ad67ebb8b8c106a7e0ea7dfc
Vimax <it-vimax@gmail.com>
2020-03-01 11:24:55 +0200

    LESSONS 17. Added functional to delete favorite item.

--- app/src/main/java/app/vimax/androiddrinkshop/FavoriteListActivity.java
+++ app/src/main/java/app/vimax/androiddrinkshop/FavoriteListActivity.java
@@ -1,27 +1,41 @@
 package app.vimax.androiddrinkshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ClipData;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 import app.vimax.androiddrinkshop.Adapter.FavoriteAdapter;
 import app.vimax.androiddrinkshop.Database.ModelDB.Favorite;
 import app.vimax.androiddrinkshop.Utils.Common;
+import app.vimax.androiddrinkshop.Utils.RecyclerItemTouchHelper;
+import app.vimax.androiddrinkshop.Utils.RecyclerItemTouchHelperListener;
 import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
-public class FavoriteListActivity extends AppCompatActivity {
+public class FavoriteListActivity extends AppCompatActivity implements RecyclerItemTouchHelperListener {
     RecyclerView recycler_fav;
 
+    RelativeLayout rootLayout;
     CompositeDisposable compositeDisposable;
+    FavoriteAdapter favoriteAdapter;
+    List<Favorite> localFavorites = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,27 +44,75 @@ public class FavoriteListActivity extends AppCompatActivity {
 
         compositeDisposable = new CompositeDisposable();
 
+        rootLayout = (RelativeLayout) findViewById(R.id.rootLayout);
+
         recycler_fav = (RecyclerView) findViewById(R.id.recycler_fav);
         recycler_fav.setLayoutManager(new LinearLayoutManager(this));
         recycler_fav.setHasFixedSize(true);
 
+        ItemTouchHelper.SimpleCallback simpleCallback = new RecyclerItemTouchHelper(0, ItemTouchHelper.LEFT, this);
+        new ItemTouchHelper(simpleCallback).attachToRecyclerView(recycler_fav);
+
         loadFavoritesItem();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadFavoritesItem();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
     }
 
     private void loadFavoritesItem() {
         compositeDisposable.add(Common.favoriteRepository.getFavItems()
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribeOn(Schedulers.io())
-        .subscribe(new Consumer<List<Favorite>>() {
-            @Override
-            public void accept(List<Favorite> favorites) throws Exception {
-                displayFavoriteItem(favorites);
-            }
-        }));
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Consumer<List<Favorite>>() {
+                    @Override
+                    public void accept(List<Favorite> favorites) throws Exception {
+                        displayFavoriteItem(favorites);
+                    }
+                }));
     }
 
     private void displayFavoriteItem(List<Favorite> favorites) {
-        FavoriteAdapter favoriteAdapter = new FavoriteAdapter(this, favorites);
+        localFavorites = favorites;
+        favoriteAdapter = new FavoriteAdapter(this, favorites);
         recycler_fav.setAdapter(favoriteAdapter);
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
+        if (viewHolder instanceof FavoriteAdapter.FavoriteViewHolder) {
+            String name = localFavorites.get(viewHolder.getAdapterPosition()).name;
+
+            final Favorite deleteItem = localFavorites.get(viewHolder.getAdapterPosition());
+            final int deleteIndex = viewHolder.getAdapterPosition();
+
+            //delete item from adapter
+            favoriteAdapter.removeItem(deleteIndex);
+            //delete item from room database
+            Common.favoriteRepository.delete(deleteItem);
+
+            Snackbar snackbar = Snackbar.make(
+                    rootLayout,
+                    new StringBuilder(name).append(" removed from Favorites List").toString(),
+                    Snackbar.LENGTH_LONG
+            );
+            snackbar.setAction("UNDO", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    favoriteAdapter.restoreItem(deleteItem, deleteIndex);
+                    Common.favoriteRepository.insertFav(deleteItem);
+                }
+            });
+            snackbar.setActionTextColor(Color.YELLOW);
+            snackbar.show();
+        }
     }
 }
